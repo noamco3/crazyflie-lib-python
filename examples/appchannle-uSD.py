e83f454166632b6d788a2b6b2a326667f092e0f2
noamco3 <noamco67@gmail.com>
2021-02-11 09:51:14 +0200

    added notes and documantation

--- examples/appchannle-uSD.py
+++ examples/appchannle-uSD.py
@@ -49,7 +49,7 @@ class AppchannelTest:
     """Example that connects to a Crazyflie and ramps the motors up/down and
     the disconnects"""
 
-    def __init__(self, link_uri):
+    def __init__(self, link_uri): 
         """ Initialize and run the example with the specified link_uri """
 
         self._cf = Crazyflie()
@@ -87,9 +87,13 @@ class AppchannelTest:
         self._is_connected = True
     
     def _request_data(self):
-        self._running = True
-        self._cf.param.set_value('usd.sendAppChannle', 1)
-        self._time = datetime.datetime.now()
+        """
+        sets the sendAppChannle to signafy to the crazyflie to start sending data.
+        tell the class to start running (set _time to now and _running to true)
+        """
+        self._running = True #set itself as running
+        self._cf.param.set_value('usd.sendAppChannle', 1) #signafy to the crazyflie to start sending data. the parameter will be set back to false (0) by the cf
+        self._time = datetime.datetime.now() #start timer to see how long is he download
     
     def _wait_for_connection(self, timeout=10):
         """
@@ -109,6 +113,9 @@ class AppchannelTest:
             time.sleep(0.5)
     
     def _disconnect(self):
+        """
+        closes link with the crazyflie
+        """
         if(self._is_connected):
             self._cf.param.set_value('usd.sendAppChannle', 0)
             self._is_connected = False
@@ -137,41 +144,49 @@ class AppchannelTest:
         print('Disconnected from %s' % link_uri)
 
     def _app_packet_received(self, data):
-        (msgType, addr) = struct.unpack("<BI", data[:5])
-        #print(msgType, addr)
+        """
+        Callback when a packet is recived through appchannel.
+        deals with the data sent. gets log file size and data and saves it to file.
+        """
+        (msgType, addr) = struct.unpack("<BI", data[:5])    #get the header of the msg. the first byte is the type the second is the memory address/file size.
+                                                            #type 0 - the log file size. addr represents size. requires to send ack back.
+                                                            #type 1 - data. addr represents the adress the data sent starts at in the log file. 
+                                                                #after the addr is the data itself. maximum of 25 bytes. do not send ack back.
+                                                            #type 2 - data ack request. requests to send back an acknolagment that the data sent was properly recived.
+                                                                #addr represents the next address that needs to be sent (or the total file size if end is reached)
+                                                                #requires to send ack back.
 
-        if(not self._running):
-            if(msgType != 1):
-                s_data = struct.pack("<BI", 3, 0)
+        if(not self._running): #in case not running (not supposed to recieve data).
+            if(msgType != 1): #if the msg isnt a data type (1), send response. (data msg arent supposed to be responded to).
+                s_data = struct.pack("<BI", 3, 0) #send ack type 3. meaning the client isnt runnig and to stop sending log.
                 self._cf.appchannel.send_packet(s_data)
-        elif(msgType == 0):
-            if(self._data_len != 0):
-                s_data = struct.pack("<BI", 1, 0)
+        elif(msgType == 0): #type 0 - size
+            if(self._data_len != 0):    #if self._data_len isnt zero. that means you are in the middle or reciving data and not supposed to to get size
+                s_data = struct.pack("<BI", 1, 0) #send ack type 1. meaning wropng type of msg was sent.
                 self._cf.appchannel.send_packet(s_data)
-            else:
+            else:                       #otherwise
                 self._data_len = addr
-                s_data = struct.pack("<BI", 0, 0)
+                s_data = struct.pack("<BI", 0, 0) #send ack type 0. success
                 self._cf.appchannel.send_packet(s_data)
-        elif(msgType == 1):    
-            if(len(data) > 5):
+        elif(msgType == 1): #type 1 - data
+            if(len(data) > 5): #if contains data
                 r_data = data[5:]
-                if(addr == len(self._data)):
-                    self._data += r_data
-                    #print("{} / {}".format(len(self._data), self._data_len))
-        elif(msgType == 2):
-            if(addr != len(self._data)):
-                s_data = struct.pack("<BI", 2, len(self._data))
+                if(addr == len(self._data)): #if data sent is of the address that suppoesed to be sent
+                    self._data += r_data #add to data
+        elif(msgType == 2): #type 2 - data ack request
+            if(addr != len(self._data)):    #if not on the same adderss as crazyflie (meaning some data was missed along te way)
+                s_data = struct.pack("<BI", 2, len(self._data)) #send ack type 2. meaning not all data recived. addr represents the address the data was first lost on. the next data batch will start from there.
                 self._cf.appchannel.send_packet(s_data)
-            else:
-                print("{} / {}".format(len(self._data), self._data_len))
-                if(self._data_len == len(self._data)):
-                    with open('logapp.bin', 'wb') as the_file:
+            else:                           #if data recived properly
+                print("{} / {}".format(len(self._data), self._data_len)) #print progress
+                if(self._data_len == len(self._data)): #if all reached the end of the file (all total data recived)
+                    with open('logapp.bin', 'wb') as the_file: #write data to file
                         the_file.write(self._data)
-                    self._running = 0
-                    print("time_t   ", (datetime.datetime.now() - self._time).total_seconds())
+                    self._running = 0 #stop running
+                    print("time_t   ", (datetime.datetime.now() - self._time).total_seconds()) #print total time it took for data to recive
                     self._data_len = 0
                     self._data = bytearray()
-                s_data = struct.pack("<BI", 0, len(self._data))
+                s_data = struct.pack("<BI", 0, len(self._data)) #send ack type 0. success. addr means the addr currently on.
                 self._cf.appchannel.send_packet(s_data)
 
 
@@ -222,23 +237,21 @@ if __name__ == '__main__':
     # Scan for Crazyflies and use the first one found
     
     
-    radio_uri = scan()
-    if radio_uri is None:
+    uri = scan() #get and choose the uri
+    if uri is None:
         print('None found.')
         sys.exit(1)
 
-    le = AppchannelTest(radio_uri)
-    connected = le._wait_for_connection()
+    le = AppchannelTest(uri)
+    connected = le._wait_for_connection() #waint for propper connection.
     ans = None
     
     if not connected:
         print('Connection failed.')
     else:
         while(ans != 'exit'):
-            le._request_data()
-            #t = datetime.datetime.now()
-            #print((datetime.datetime.now() - t).total_seconds())
-            while(le._running):
-                ans = input()
+            le._request_data() #signafy for crazyflie to start sending data.
+            while(le._running): #wait till finished
+                ans = input() #for whatever reason, an empty while (pass) slows the program alot more than simply input()
     
     le._disconnect()
